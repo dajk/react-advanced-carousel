26d77381e0b4d8dce65815fcb934bce95a4b9762
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2019-07-05 23:32:25 +0200

    fix(styled-components): remove sc from code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react'
-import styled from 'styled-components'
 
 interface ICarousel {
   children?: React.ReactChild
 }
 
-const Wrapper = styled.div`
-  border: 1px solid red;
-  background-color: green;
-`
-
 const Carousel = (props: ICarousel) => {
-  return <Wrapper>{props.children}</Wrapper>
+  return <div>{props.children}</div>
 }
 
 Carousel.defaultProps = {}
