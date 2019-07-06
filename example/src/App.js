d5fe33fa79e05e0d892a602dde7ec2665a634831
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2019-07-06 15:17:21 +0200

    feat(visibleItems): props for how many items are visible on screen + fix react version on example

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Carousel from 'react-advanced-carousel'
+import { items } from './items'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Carousel>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-      </Carousel>
+      <div style={{ border: '1px solid red' }}>
+        <Carousel visibleItems={3}>
+          {items.map((item, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  border: '1px solid blue',
+                  height: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                {item.name}
+              </div>
+            )
+          })}
+        </Carousel>
+      </div>
     )
   }
 }
